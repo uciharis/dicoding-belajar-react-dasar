f0e7b6004f786efc3298217e434d8ca2f6d1e611
the hengker <harisnop@gmail.com>
2024-09-25 00:05:44 +0700

    Create 1_car.js
    react component mengembalikan UI dalam bentuk react element

---
+++ 1_car.js
@@ -0,0 +1,12 @@
+function Car({manufacture, type, color}){
+return (
+      <div class='car-info'>
+  <dt>Manufaktur: </dt>
+  <dd></dd>{manufacture}</dd>
+  <dt>Type: </dt>
+  <dd>{type}</dd>
+  <dt>Color:</dt>
+  <dd>{color}</dd>
+  </div>
+  );
+}
