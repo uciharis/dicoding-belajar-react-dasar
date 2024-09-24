651edcfd4e116e9dfb9026e5d99383830321dfe5
the hengker <harisnop@gmail.com>
2024-09-25 00:18:30 +0700

    Update 1_composition.js

--- 1_composition.js
+++ 1_composition.js
@@ -1,3 +1,18 @@
 function getProfPict(userID){
   return `https://avatars.githubusercontent.com/u/${userID}`;
 }
+
+function getProfileLink(username){
+  return `https://github.com/${username}`;
+}
+
+// contoh komposisi : beberapa fungsi dipanggil utk buat data yang kompleks
+
+function getGithubInfo(username, userID){
+  return {
+    profile_link : getProfileLink(username),
+    profile_pict : getProfPict(userID)
+  };
+}
+
+console.log(getGithubInfo('mulyono',25523432));
